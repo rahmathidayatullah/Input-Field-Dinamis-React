797e20ac65ad49b7b4e5299c3c9f255f3900a1d5
rahmathidayatullah <rahmathidayatullah996@gmail.com>
2021-01-20 15:03:41 +0700

    clear simple-input-field-dinamic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
+  const [inputList, setinputList] = useState([
+    {
+      firstName: 'test',
+      lastName: 'testt',
+    },
+    {
+      firstName: 'test2',
+      lastName: 'testt2',
+    },
+  ])
+
+  const handleChange = (e, index) => {
+    const { name, value } = e.target
+    const list = [...inputList]
+    list[index][name] = value
+    setinputList(list)
+
+    setinputList(list)
+  }
+
+  const handleAddInput = () => {
+    setinputList([...inputList, { firstName: '', lastName: '' }])
+  }
+
+  const handleRemove = (index) => {
+    const list = [...inputList]
+    list.splice(index, 1)
+    setinputList(list)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <b>Input Filed Dinamis</b>
+      {inputList.map((item, i) => {
+        return (
+          <div className="box" key={i}>
+            <input
+              type="text"
+              name="firstName"
+              value={item.firstName}
+              placeholder="First Name"
+              className="mr-10"
+              onChange={(e) => handleChange(e, i)}
+            />
+            <input
+              type="text"
+              name="lastName"
+              value={item.lastName}
+              placeholder="Last Name"
+              className="mr-10"
+              onChange={(e) => handleChange(e, i)}
+            />
+            {inputList.length !== 1 && (
+              <input
+                type="button"
+                value="Remove"
+                className="mr-10"
+                onClick={() => handleRemove(i)}
+              />
+            )}
+
+            {inputList.length - 1 === i && (
+              <input type="button" value="Add" onClick={handleAddInput} />
+            )}
+          </div>
+        )
+      })}
+      <pre>{JSON.stringify(inputList, null, 2)}</pre>
     </div>
   )
 }
